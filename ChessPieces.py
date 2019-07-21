c0dc060ec4ab58ac5cf946a1dd32dc57f94761d7
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-07-21 18:41:52 +0200

    Update ChessPieces.py

--- ChessPieces.py
+++ ChessPieces.py
@@ -36,10 +36,9 @@ class Queen(ChessPiece):
         return str(self.color) + ' ♕'
 
     def get_possible_moves(self):
-        possible_moves = [[0, i+1] for i in range(7)] + [[0, -i-1] for i in range(7)] + \
-                         [[i+1, 0] for i in range(7)] + [[-i-1, 0] for i in range(7)] + \
-                         [[i+1, i+1] for i in range(7)] + [[i+1, -i-1] for i in range(7)] + \
-                         [[-i-1, i+1] for i in range(7)] + [[-i-1, -i-1] for i in range(7)]
+        possible_moves = [[0, i-7] for i in range(16) if i != 7] + [[i-7, 0] for i in range(16) if i != 7] + \
+                         [[i-7, i-7] for i in range(16) if i != 7] + \
+                         [[-i-1, i+1] for i in range(7)] + [[i+1, -i-1] for i in range(7)]
         for moves in possible_moves:
             moves[0] += self.x
             moves[1] += self.y
@@ -49,13 +48,13 @@ class Queen(ChessPiece):
 class Rock(ChessPiece):
     def __init__(self, color, position):
         super().__init__(color, position)
+        self.has_moved = False
 
     def __repr__(self):
         return str(self.color) + ' ♖'
 
     def get_possible_moves(self):
-        possible_moves = [[0, i+1] for i in range(7)] + [[0, -i-1] for i in range(7)] + \
-                         [[i+1, 0] for i in range(7)] + [[-i-1, 0] for i in range(7)]
+        possible_moves = [[0, i-7] for i in range(16) if i != 7] + [[i-7, 0] for i in range(16) if i != 7]
         for moves in possible_moves:
             moves[0] += self.x
             moves[1] += self.y
@@ -85,8 +84,8 @@ class Bishop(ChessPiece):
         return str(self.color) + ' ♗'
 
     def get_possible_moves(self):
-        possible_moves = [[i+1, i+1] for i in range(7)] + [[i+1, -i-1] for i in range(7)] + \
-                         [[-i-1, i+1] for i in range(7)] + [[-i-1, -i-1] for i in range(7)]
+        possible_moves = [[i-7, i-7] for i in range(16) if i != 7] + \
+                         [[-i-1, i+1] for i in range(7)] + [[i+1, -i-1] for i in range(7)]
         for moves in possible_moves:
             moves[0] += self.x
             moves[1] += self.y
@@ -98,22 +97,20 @@ class Pawn(ChessPiece):
         super().__init__(color, position)
         self.direction = direction
         self.has_moved = False
+        self.en_passant = False
 
     def __repr__(self):
         return str(self.color) + ' ♙'
 
     def get_possible_moves(self):
         possible_moves = [[-1, 1], [0, 1], [1, 1], [1, -1], [0, -1], [-1, -1]]
-        if self.direction is UP:
-            for moves in possible_moves[0:3]:
-                moves[0] += self.x
-                moves[1] += self.y
-            return possible_moves[0:3]
-        elif self.direction is DOWN:
-            for moves in possible_moves[3:6]:
-                moves[0] += self.x
-                moves[1] += self.y
-            return possible_moves[3:6]
+        for moves in possible_moves:
+            moves[0] += self.x
+            moves[1] += self.y
+            if self.direction is UP:
+                return possible_moves[0:3]
+            elif self.direction is DOWN:
+                return possible_moves[3:6]
 
 
 if __name__ == '__main__':
